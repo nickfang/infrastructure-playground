5045a31411e91108513f36995ab27a6d7ae97a0f
Nick Fang <fang.nicholas@gmail.com>
2025-06-27 16:51:07 -0500

    Making the service runnable.

--- node-microservice/index.js
+++ node-microservice/index.js
@@ -5,18 +5,18 @@ const app = express();
 const port = process.env.PORT | 3001;
 
 app.get('/products', (req, res) => {
-   res.status(200).json({
-      products: [],
-   });
+  res.status(200).json({
+    products: [],
+  });
 });
 
 app.get('/health', (req, res) => {
-   res.status(200).json({
-      status: 'UP',
-      uptime: process.uptime()
-   });
+  res.status(200).json({
+    status: 'UP',
+    uptime: process.uptime(),
+  });
 });
 
-app.listen(PORT, () => {
-   console.log(`Product Service is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Product Service is running on port ${port}`);
 });
